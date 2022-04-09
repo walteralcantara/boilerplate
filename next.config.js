d01524c98696104285caf9de7363bb66e68d7132
Walter Alcantara <waltermalcantara@gmail.com>
2022-04-09 18:45:42 -0300

    Added Next PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+const isProduction = process.env.NODE_ENV === "production";
+
+const nextConfig = withPWA({
+  pwa: {
+    dest: "public",
+    disable: !isProduction
+  },
+  reactStrictMode: true
+});
+
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Walter - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, Next.js and Styled Components"
