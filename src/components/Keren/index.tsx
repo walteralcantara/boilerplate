aad717a72ad7c1acfdb73df4a3fc15f09bd9589d
Walter Alcantara <waltermalcantara@gmail.com>
2022-04-09 20:32:57 -0300

    Removed a test for plop generator file component

--- src/components/Keren/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from "./styled";
-
-const Keren = () => (
-  <S.Wrapper>
-    <S.Title>Keren</S.Title>
-  </S.Wrapper>
-);
-
-export default Keren;
--- src/components/Keren/styled.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Wrapper = styled.main``;
-
-export const Title = styled.h1`
-  font-size: 2.5rem;
-`;
